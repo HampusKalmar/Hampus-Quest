14eadc9cb3eee169fb85dfb69e06f7cbd4432172
Hampus Tuisku <ht222mi@student.lnu.se>
2023-05-29 10:32:29 +0200

    Add automatic test

---
+++ test/player-test.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <gtest/gtest.h>
+#include "../include/player.h"
+
+class PlayerPositionTest : public ::testing::Test
+{
+  public:
+    virtual void SetUp()
+    {
+      testWindow = new sf::RenderWindow(sf::VideoMode(1000, 900), "Test Window");
+      texture.loadFromFile("assets/images/newSpriteOne.png");
+      sprite.setTexture(texture);
+      sprite.setScale(2.0f, 2.0f);
+      sprite.setPosition(50, 700);
+      player.getSprite() = sprite;
+
+    }
+  public:
+    virtual void TearDown()
+    {
+      delete testWindow;
+    }
+  public:
+    sf::Texture texture;
+    sf::RenderWindow* testWindow;
+    sf::Sprite sprite;
+    Player player;
+    
+};
+
+TEST_F(PlayerPositionTest, PlayerStartingPosition)
+{
+  sf::Vector2f expectedPosition(50, 700);
+  EXPECT_NEAR(expectedPosition.x, player.getSprite().getPosition().x, 0);
+}
+
+int main(int argc, char** argv)
+{
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
